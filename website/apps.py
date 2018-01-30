65ecf4cd40fb39a0048c4f22be5874ce29fb632e
허재영 <jay.jaeyoung@gmail.com>
2018-01-30 23:27:35 +0900

    Delete all the previous version legacy

--- website/apps.py
+++ website/apps.py
@@ -3,7 +3,3 @@ from django.apps import AppConfig
 
 class WebsiteConfig(AppConfig):
     name = 'website'
-
-    # This will trigger the @receiver decorator in singals.py
-    def ready(self):
-        import website.signals
